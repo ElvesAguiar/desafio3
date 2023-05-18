46540fe3b3fd3a6ba7373a47e3dc464dd10b04f6
Elves Aguiar <elvesmhs@hotmail.com>
2023-05-18 11:17:45 -0300

    feat: databese seed

---
+++ src/main/resources/import.sql
@@ -0,0 +1,19 @@
+INSERT INTO tb_client  (birth_date, children , cpf , income, name) VALUES ('1985-09-15', 2, '32145685265', 1500.0 , 'Jung cook' );
+
+INSERT INTO tb_client  (birth_date, children , cpf , income, name) VALUES ('1996-10-03', 1, '32145685265', 1700.0 , 'Takakara Nomuru' );
+
+INSERT INTO tb_client  (birth_date, children , cpf , income, name) VALUES ('1983-09-15', 2, '35795145869', 2300.0 , 'Alexandra Smith' );
+
+INSERT INTO tb_client  (birth_date, children , cpf , income, name) VALUES ('1990-08-28', 0, '89657412359', 900.0 , 'Caramuru Tacumpira' );
+
+INSERT INTO tb_client  (birth_date, children , cpf , income, name) VALUES ('2010-01-30', 0, '45685214785', 1750.0 , 'Enzo Aguiar' );
+
+INSERT INTO tb_client  (birth_date, children , cpf , income, name) VALUES ('2000-02-14', 1, '98745625875', 2300.0 , 'Juliana Ferraz' );
+
+INSERT INTO tb_client  (birth_date, children , cpf , income, name) VALUES ('1996-06-12', 1, '85245635741', 1250.0 , 'Maria de Jesus' );
+
+INSERT INTO tb_client  (birth_date, children , cpf , income, name) VALUES ('1989-03-03', 2, '85245635741', 2985.0 , 'Felix Assunção' );
+
+INSERT INTO tb_client  (birth_date, children , cpf , income, name) VALUES ('1985-09-18', 3, '85245635741', 3680.0 , 'Maria do Carmo' );
+
+INSERT INTO tb_client  (birth_date, children , cpf , income, name) VALUES ('1969-08-06', 3, '75698412563', 1689.0 , 'João de Deus' );
